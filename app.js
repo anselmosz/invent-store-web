56598c98a9bbac4b0d19e8d50b1aa8d97d262fa5
anselmosz <anselmoma2005@outlook.com>
2025-04-03 09:50:15 -0300

    added js script at project

---
+++ app.js
@@ -0,0 +1,7 @@
+const nome = document.getElementById("nome");
+const categoria = document.getElementById("categoria");
+const preco = document.getElementById("preco");
+const quantidade = document.getElementById("quantidade");
+const imagem = document.getElementById("imagem");
+
+
