3b249376b3ecbad38226d30f57737e706f5a2d34
anselmosz <anselmoma2005@outlook.com>
2025-04-03 11:07:46 -0300

    added error treat in js script

--- app.js
+++ app.js
@@ -3,5 +3,32 @@ const categoria = document.getElementById("categoria");
 const preco = document.getElementById("preco");
 const quantidade = document.getElementById("quantidade");
 const imagem = document.getElementById("imagem");
+const produtoForm = document.getElementById("produto-form")
 
+produtoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  if (nome.value == "") {
+    document.getElementById("erro-nome").style.display = 'block';
+  } else {
+    document.getElementById("erro-nome").style.display = 'none';
+  }
+
+  if (categoria.value == "") {
+    document.getElementById("erro-categoria").style.display = 'block';
+  } else {
+    document.getElementById("erro-categoria").style.display = 'none';
+  }
+
+  if (preco.value == "") {
+    document.getElementById("erro-preco").style.display = 'block';
+  } else {
+    document.getElementById("erro-preco").style.display = 'none';
+  }
+
+  if (quantidade.value == "") {
+    document.getElementById("erro-quantidade").style.display = 'block';
+  } else {
+    document.getElementById("erro-quantidade").style.display = 'none';
+  }
+});
