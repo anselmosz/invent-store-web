60a9940c728fb40926981e48e30a244baf33c7d7
anselmosz <anselmoma2005@outlook.com>
2025-05-09 11:13:33 -0300

    update project by change the notifications logic

--- app.js
+++ app.js
@@ -5,53 +5,74 @@ const quantidade = document.getElementById("quantidade");
 const imagem = document.getElementById("imagem");
 const produtoForm = document.getElementById("produto-form");
 
-const notificacao = document.getElementById("notificacao");
+const notificacao = document.getElementById("notificacao-content");
+
 notificacao.style.display = 'none';
 
 const produtos = [];
 
-function exibirNotificacao() {
+function exibirNotificacao(mensagem, status) {
+  const messageEl = document.getElementById("notificacao-msg");
   
+  // o textConte é responsável por alterar o texto guardado na variável
+  messageEl.textContent = mensagem;
+  
+  if (status === "sucesso") {
+    notificacao.style.backgroundColor = '#b7d5ac';
+    messageEl.style.color = '#103900';
+  } else if (status === "alerta") {
+    notificacao.style.backgroundColor = '#ffffa0';
+    messageEl.style.color = '#646600';
+  } else if (status === "erro") {
+    notificacao.style.backgroundColor = '#fb6866';  
+    messageEl.style.color = '#103900';
+  }
+  
+  notificacao.style.display = 'block';
+  
+  setTimeout(() => {
+    notificacao.style.display = 'none';
+  }, 2000);
 }
 
 produtoForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let camposPreenchidos = true
-
+  
   if (nome.value == "") {
     document.getElementById("erro-nome").style.display = 'block';
     camposPreenchidos = false;
   } else {
     document.getElementById("erro-nome").style.display = 'none';
   }
-
+  
   if (categoria.value == "") {
     document.getElementById("erro-categoria").style.display = 'block';
     camposPreenchidos = false;
   } else {
     document.getElementById("erro-categoria").style.display = 'none';
   }
-
+  
   if (preco.value == "") {
     document.getElementById("erro-preco").style.display = 'block';
     camposPreenchidos = false;
   } else {
     document.getElementById("erro-preco").style.display = 'none';
   }
-
+  
   if (quantidade.value == "") {
     document.getElementById("erro-quantidade").style.display = 'block';
     camposPreenchidos = false;
   } else {
     document.getElementById("erro-quantidade").style.display = 'none';
   }
-
+  
   if (camposPreenchidos == false){
-    notificacao.style.display = 'none';
+    exibirNotificacao("erro ao inserir o produto","erro");
     return;
   }
-
+  
   const produtoInserido = {
     nome : nome.value,
     categoria : categoria.value,
@@ -62,6 +83,8 @@ produtoForm.addEventListener("submit", (event) => {
   produtos.push(produtoInserido);
   
   localStorage.setItem("produtoInfo", JSON.stringify(produtos));
+
+  exibirNotificacao("Produto inserido com sucesso!","sucesso");
   
   produtoForm.reset();
 });
